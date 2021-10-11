c4768c627af0f453093139a5ecfc0827b7b5fac6
paulahemsi <paulahemsi@gmail.com>
2021-10-10 21:10:02 -0300

    meoow woof

--- module_04/ex00/Dog.cpp
+++ module_04/ex00/Dog.cpp
@@ -6,35 +6,14 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 14:42:21 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/10/10 18:37:43 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/10/10 21:08:33 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Dog.hpp"
-# define RED			"\e[0;31m"
-# define L_RED			"\e[0;38;5;9m"
-# define GREEN			"\e[0;32m"
-# define D_GREEN		"\e[0;38;5;22m"
-# define V_GREEN		"\e[0;38;5;82m"
-# define P_GREEN		"\e[0;38;5;23m"
-# define BLUE			"\e[0;34m"
-# define V_BLUE			"\e[0;38;5;27m"
-# define P_BLUE			"\e[0;38;5;24m"
+
 # define CYAN			"\e[0;36m"
-# define V_CYAN			"\e[0;38;5;44m"
-# define MAGENTA		"\e[0;35m"
-# define PURPLE			"\e[0;38;5;93m"
-# define LILAC			"\e[0;38;5;13m"
-# define PINK			"\e[0;38;5;199m"
-# define YELLOW			"\e[0;33m"
-# define L_YELLOW		"\e[0;38;5;191m"
-# define BLACK			"\e[0;30m"
-# define WHITE			"\e[0;37m"
-# define GRAY			"\e[0;38;5;8m"
-# define L_GRAY			"\e[0;38;5;245m"
-# define ORANGE			"\e[0;38;5;166m"
-# define V_ORANGE		"\e[0;38;5;202m"
 
 Dog::Dog(void) : Animal("Dog")
 {
@@ -44,7 +23,7 @@ Dog::Dog(void) : Animal("Dog")
 
 Dog::Dog(Animal const& instance) : Animal(instance)
 {
-	std::cout << CYAN << "Heey loook! Thats nice, now we're twooo dog! Uhuuu!" << std::endl;
+	std::cout << CYAN << "Heey loook! Thats nice, now we're twooo dogys! Uhuuu!" << std::endl;
 	return ;
 }
 
@@ -62,6 +41,6 @@ Dog	&Dog::operator=(Dog const &right_hand_side)
 
 void Dog::makeSound(void)const
 {
-	std::cout << CYAN << "rrrrr  aaaaau!" << std::endl;
+	std::cout << CYAN << "rrrrr woof woof woooof!" << std::endl;
 	return ;
 }
--- module_04/ex00/WrongCat.cpp
+++ module_04/ex00/WrongCat.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 14:51:50 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/10/10 18:30:48 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/10/10 21:09:36 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@
 
 WrongCat::WrongCat(void) : WrongAnimal("WrongCat")
 {
-	std::cout << L_GRAY << "who arrre you and hou you darrrre bother meeeau?" << std::endl;
+	std::cout << L_GRAY << "who arrre you and hou you darrrre bother meeeow?" << std::endl;
 	return ;
 }
 
 WrongCat::WrongCat(WrongCat const& instance)
 {
 	*this = instance;
-	std::cout << L_GRAY << "miiiiiau... why another Wrongcat, stupid human?" << std::endl;
+	std::cout << L_GRAY << "meeow... why another Wrongcat, stupid human?" << std::endl;
 	return ;
 }
 
 WrongCat::~WrongCat(void)
 {
-	std::cout << L_GRAY << "farewell" << std::endl;
+	std::cout << L_GRAY << "farewell, human,and serve me better next timeeeeow" << std::endl;
 	return ;
 }
 
@@ -43,6 +43,6 @@ WrongCat	&WrongCat::operator=(WrongCat const &right_hand_side)
 
 void WrongCat::makeSound(void)const
 {
-	std::cout << L_GRAY << "miiiiiau!" << std::endl;
+	std::cout << L_GRAY << "meoow!" << std::endl;
 	return ;
 }
