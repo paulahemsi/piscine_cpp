01eaf092eaf7633da2011862a5c696ef7e73d8a6
paulahemsi <paulahemsi@gmail.com>
2021-10-13 18:57:25 -0300

    polish(main)

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 13:09:26 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/10/10 22:38:22 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/10/13 18:52:51 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,30 @@ static void	tic_tac(int usleep_time, int duration)
 int main(void)
 {
 	const Animal* j = new Dog();
+	tic_tac(500000, 5);
 	const Animal* i = new Cat();
-	delete j;//should not create a leak
+	tic_tac(500000, 5);
+	delete j;
+	tic_tac(500000, 5);
 	delete i;
-		tic_tac(100000, 5);
+	tic_tac(500000, 5);
 	Animal *animals[10];
+	tic_tac(500000, 5);
 	for(int i = 0; i < 10; i++)
 	{
 		if (i % 2 == 0)
 			animals[i] = new Cat;
 		else
 			animals[i] = new Dog;
+		tic_tac(100000, 5);
 	}
 	for(int i = 0; i < 10; i++)
 	{
 		animals[i]->makeSound();
+		tic_tac(500000, 5);	
 		animals[i]->getIdea();
+		tic_tac(500000, 5);
 		delete animals[i];
+		tic_tac(500000, 5);
 	}
 }
--- module_04/ex02/main.cpp
+++ module_04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 13:09:26 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/10/11 22:21:38 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/10/13 18:55:46 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,30 @@ static void	tic_tac(int usleep_time, int duration)
 
 int main(void)
 {
-	const Animal* dog = new Dog();
+	const Animal* j = new Dog();
 	tic_tac(500000, 5);
-	const Animal* cat = new Cat();
+	const Animal* i = new Cat();
 	tic_tac(500000, 5);
-	delete dog;//should not create a leak
+	delete j;
 	tic_tac(500000, 5);
-	delete cat;
+	delete i;
 	tic_tac(500000, 5);
-	Animal *animals[10];
+	Animal *animals[4];
 	tic_tac(500000, 5);
-	for(int i = 0; i < 10; i++)
+	for(int i = 0; i < 4; i++)
 	{
 		if (i % 2 == 0)
 			animals[i] = new Cat;
 		else
 			animals[i] = new Dog;
-		tic_tac(500000, 5);
+		tic_tac(100000, 5);
 	}
-	tic_tac(500000, 5);
-	for(int i = 0; i < 10; i++)
+	for(int i = 0; i < 4; i++)
 	{
 		animals[i]->makeSound();
+		tic_tac(500000, 5);	
 		animals[i]->getIdea();
+		tic_tac(500000, 5);
 		delete animals[i];
 		tic_tac(500000, 5);
 	}
