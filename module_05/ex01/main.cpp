eb568c7a1dcc19bb46f899debc4cdfd44a0c3207
paulahemsi <paulahemsi@gmail.com>
2021-10-16 20:21:47 -0300

    refactor(music): remove sound

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 19:21:27 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/10/15 21:44:44 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/10/16 20:20:23 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ static void	tic_tac(int usleep_time, int duration)
 
 int main(void)
 {
-	system("canberra-gtk-play -f audio.ogg");
 	std::cout << std::endl << "----Let the bureaucracy begin----" << std::endl << std::endl;
 	tic_tac(500000, 5);
 	Bureaucrat	john;
@@ -37,6 +36,8 @@ int main(void)
 	Form		form42("42", 42, 42);
 	tic_tac(500000, 5);
 	Form		form149("149", 149, 149);
+	tic_tac(500000, 5);
+	Form		form77_61("77_61", 77, 61);
 	tic_tac(500000, 5);
 	std::cout << john << std::endl;
 	tic_tac(500000, 5);
