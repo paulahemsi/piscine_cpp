a8919d5071dca5a1325331c7ee52fbd0ffe28aa0
paulahemsi <paulahemsi@gmail.com>
2021-10-16 21:56:34 -0300

    refactor(tests): m05 ex02 tests update

--- module_05/ex02/main.cpp
+++ module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 19:21:27 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/10/16 01:30:36 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/10/16 21:54:01 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,22 @@ int main(void)
 {
 	std::cout << std::endl << "----Let the bureaucracy begin----" << std::endl << std::endl;
 	
+	std::cout << "\n\n---------FORMS CREATION TEST---------\n\n" << std::endl;
+
 	PresidentialPardonForm form("the imposible future");
 	tic_tac(500000, 5);
 	ShrubberyCreationForm form2("my_beautyfull_ASCII_trees");
 	tic_tac(500000, 5);
 	RobotomyRequestForm form3("someone");
 	tic_tac(500000, 5);
+
+	std::cout << "\n\n---------SIGN AND EXECUTE TEST---------\n\n" << std::endl;
+
 	Bureaucrat	john;
 	tic_tac(500000, 5);
 	Bureaucrat	fred("fred", 1);
 	tic_tac(500000, 5);
+	
 	john.executeForm(form);
 	tic_tac(500000, 5);
 	fred.executeForm(form);
@@ -48,6 +54,11 @@ int main(void)
 	fred.executeForm(form2);
 	tic_tac(500000, 5);
 	fred.executeForm(form3);
+	tic_tac(500000, 5);
+
+	john.signForm(form);
+	tic_tac(500000, 5);
+	std::cout << form << std::endl;
 	tic_tac(500000, 5);
 	fred.signForm(form);
 	tic_tac(500000, 5);
@@ -55,6 +66,7 @@ int main(void)
 	tic_tac(500000, 5);
 	fred.signForm(form2);
 	tic_tac(500000, 5);
+	
 	john.executeForm(form);
 	tic_tac(500000, 5);
 	fred.executeForm(form);
@@ -63,10 +75,14 @@ int main(void)
 	tic_tac(500000, 5);
 	fred.executeForm(form2);
 	tic_tac(500000, 5);
+
+	std::cout << "\n\n---------NEW INCREMENT DECREMENT CHECK TEST---------\n\n" << std::endl;
+
 	std::cout << john << std::endl;
 	tic_tac(500000, 5);
 	std::cout << fred << std::endl;
 	tic_tac(500000, 5);
+	
 	fred.incrementGrade();
 	fred.incrementGrade();
 	fred.incrementGrade();
@@ -74,6 +90,7 @@ int main(void)
 	john.decrementGrade();
 	john.decrementGrade();
 	tic_tac(500000, 5);
+	
 	std::cout << fred << std::endl;
 	tic_tac(500000, 5);
 	std::cout << john << std::endl;
