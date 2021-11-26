f701c6f63682483cf383b798d58dc55b7c1869a1
paulahemsi <paulahemsi@gmail.com>
2021-11-26 20:26:22 -0300

    tests

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:20:29 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/11/26 17:38:23 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/11/26 20:25:12 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ class Array
 		Array(void): _n(0), _ptr(0) {};
 		Array(unsigned int n): _n(n), _ptr(new T[n]()) {};
 
-		Array(Array &instance): _n(instance.size()), _ptr(new T[this->_n]())
+		Array(Array const &instance): _n(instance.size()), _ptr(new T[this->_n]())
 		{
 			*this = instance;
 		};
@@ -61,7 +61,6 @@ class Array
 			}
 			return (*this);
 		}
-		
 		unsigned int size(void) const {return (_n);};
 
 	class OutOfBoundsException : public std::exception
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 18:20:27 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/11/26 17:43:14 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/11/26 20:25:49 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int main(void)
 {
 	std::cout << V_CYAN << "\n----initializing with 0 new int array fo size 5----\n" << std::endl;
 
-	Array<int>intArray(5);
+	Array<int> intArray = Array<int>(5);
+	Array<const int> intArray2(5);
+
 
 	std::cout << V_CYAN << "\n----looping throw and trying to access out of bounds----\n" << RESET << std::endl;
 
@@ -29,12 +31,14 @@ int main(void)
 		try
 		{
 			std::cout << intArray[i] << std::endl;
+			std::cout << intArray2[i] << std::endl;
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
 	}
+
 
 	std::cout << V_CYAN << "\n----assigning values (0 - 4)----\n" << RESET << std::endl;
 
@@ -43,12 +47,14 @@ int main(void)
 		try
 		{
 			intArray[i] = i;
+			std::cout << "intArray[" << i << "] = " << i << std::endl;
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
 	}
+
 
 	std::cout << V_CYAN << "\n----loopping throw the array----\n" << RESET << std::endl;
 
@@ -64,78 +70,70 @@ int main(void)
 		}
 	}
 
-	// std::cout << V_CYAN << "\n----changing specific index value----\n" << RESET << std::endl;
+	std::cout << V_CYAN << "\n----changing specific index value----\n" << RESET << std::endl;
 
-	// std::cout << V_CYAN << "\nold value:" << RESET << std::endl;
-	// std::cout << "intArray[2]:" << intArray[2] << std::endl;
-	// intArray[2] = 42;
-	// std::cout << V_CYAN << "\nnew value (42):" << RESET << std::endl;
-	// std::cout << "intArray[2]:" << intArray[2] << std::endl;
+	std::cout << V_CYAN << "\nold value:" << RESET << std::endl;
+	std::cout << "intArray[2]:" << intArray[2] << std::endl;
+	intArray[2] = 42;
+	std::cout << V_CYAN << "\nnew value (42):" << RESET << std::endl;
+	std::cout << "intArray[2]:" << intArray[2] << std::endl;
 
-	// std::cout << V_CYAN << "\n----copy test----\n" << RESET << std::endl;
 
-	// Array<int> intArrayCopy(intArray);
-	// std::cout << "intArrayCopy[2]:" << intArrayCopy[2] << std::endl;
-	// intArray[2] = 7;
-	// std::cout << "intArray[2]:" << intArray[2] << std::endl;
-	// std::cout << "intArrayCopy[2]:" << intArrayCopy[2] << std::endl;
+	std::cout << V_CYAN << "\n----copy test----\n" << RESET << std::endl;
 
-	// std::cout << V_CYAN << "\n----testing small array atribution----\n" << RESET << std::endl;
+	Array<int> intArrayCopy(intArray);
+	std::cout << "intArrayCopy[2]:" << intArrayCopy[2] << std::endl;
+	intArray[2] = 7;
+	std::cout << "intArray[2]:" << intArray[2] << std::endl;
+	std::cout << "intArrayCopy[2]:" << intArrayCopy[2] << std::endl;
 
-	// Array<int> smallArray = Array<int>(2);
-	// smallArray = intArray;
 
-	// std::cout << V_CYAN << "\n----empty constructor and accessing test----\n" << RESET << std::endl;
+	std::cout << V_CYAN << "\n----testing array too small to copy----\n" << RESET << std::endl;
 
-	// Array<int> t2 = Array<int>();
-	// try
-	// {
-	// 	std::cout << t2[0] << std::endl;
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
+	Array<int> smallArray(2);
+	smallArray = intArray;
 
-	// std::cout << V_CYAN << "\n----char array test----\n" << RESET << std::endl;
 
-	// Array<char> constCharArray = Array<char>(2);
-	// try
-	// {
-	// 	std::cout << constCharArray[0] << std::endl;
-	// 	std::cout << constCharArray[1] << std::endl;
-	// 	std::cout << constCharArray[2] << std::endl;
-	// 	constCharArray[0] = 'c';
-	// 	std::cout << constCharArray[0] << std::endl;
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
+	std::cout << V_CYAN << "\n----empty constructor and accessing test----\n" << RESET << std::endl;
 
-	// Array<char> charArray = Array<char>(3);
+	Array<int> t2 = Array<int>();
 
-	// for (unsigned int i = 0; i < charArray.size(); i++)
-	// {
-	// 	try
-	// 	{
-	// 		charArray[i] = 'a' + i;
-	// 	}
-	// 	catch(const std::exception& e)
-	// 	{
-	// 		std::cerr << e.what() << '\n';
-	// 	}
-	// }
+	try
+	{
+		std::cout << t2[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-	// for (unsigned int i = 0; i < charArray.size(); i++)
-	// {
-	// 	try
-	// 	{
-	// 		std::cout << charArray[i] << std::endl;
-	// 	}
-	// 	catch(const std::exception& e)
-	// 	{
-	// 		std::cerr << e.what() << '\n';
-	// 	}
-	// }
+	std::cout << "t2 size: " << t2.size() << std::endl;
+
+	std::cout << V_CYAN << "\n----char array test----\n" << RESET << std::endl;
+
+	Array<char> charArray(3);
+
+	for (unsigned int i = 0; i < charArray.size(); i++)
+	{
+		try
+		{
+			charArray[i] = 'a' + i;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+
+	for (unsigned int i = 0; i < charArray.size(); i++)
+	{
+		try
+		{
+			std::cout << charArray[i] << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
 }
